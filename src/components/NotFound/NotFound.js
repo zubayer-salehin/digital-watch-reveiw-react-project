2d809c37b9d750902706e1aff89b94ad62b257f8
zubayer-salehin <zubaersalehin@gmail.com>
2022-04-04 22:45:34 +0600

    error handling

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <p>Page not found</p>
+        <div className='notFound-container'>
+            <h1>Opps!</h1>
+            <h5>404-PAGE NOT FOUND</h5>
+            <p>The page you are looking for might have been removed had its's name change or is temporarily unavailable.</p>
+            <button>GO TO HOMEPAGE</button>
         </div>
     );
 };
